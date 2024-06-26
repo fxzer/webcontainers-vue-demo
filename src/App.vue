19e1b5ea073cd3b47a197f7f1cec723a47a4f808
fxzer <fxzer8888@163.com>
2024-06-26 18:26:29 +0800

    chore: add interactivity

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,69 @@
 import { onMounted, ref, watch } from 'vue'
 import { WebContainer } from '@webcontainer/api'
 import { Terminal } from '@xterm/xterm'
+import { FitAddon } from '@xterm/addon-fit'
 import { files } from './files'
 
 const iframeSrc = ref('./src/loading.html')
 const textareaValue = ref(files['index.js'].file.contents)
-let webcontainerInstance: WebContainer
 const terminalEl = ref<HTMLDivElement >()
+
+let terminal: Terminal
+let fitAddon: FitAddon
+let webcontainerInstance: WebContainer
+
 onMounted(async () => {
   webcontainerInstance = await WebContainer.boot()
   await webcontainerInstance.mount(files)
   // await readFile()
-  const terminal = new Terminal({
-    convertEol: true,
-  })
-  if (terminalEl.value)
-    terminal.open(terminalEl.value)
+  await openTerminal()
   const exitCode = await installDependencies(terminal)
   if (exitCode !== 0) {
     throw new Error('Installation failed')
   };
-
   await startDevServer(terminal)
+  await startShell()
 })
+async function openTerminal() {
+  fitAddon = new FitAddon()
+  terminal = new Terminal({
+    convertEol: true,
+  })
+  terminal.loadAddon(fitAddon)
+  if (terminalEl.value)
+    terminal.open(terminalEl.value)
+  fitAddon.fit()
+}
+async function startShell() {
+  const shellProcess = await webcontainerInstance.spawn('jsh', {
+    terminal: {
+      cols: terminal.cols,
+      rows: terminal.rows,
+    },
+  })
+  shellProcess.output.pipeTo(
+    new WritableStream({
+      write(data) {
+        terminal.write(data)
+      },
+    }),
+  )
+
+  const input = shellProcess.input.getWriter()
+  terminal.onData((data) => {
+    input.write(data)
+  })
+
+  window.addEventListener('resize', () => {
+    fitAddon.fit()
+    shellProcess.resize({
+      cols: terminal.cols,
+      rows: terminal.rows,
+    })
+  })
+
+  return terminal
+};
 
 // async function readFile() {
 //   const packageJSON = await webcontainerInstance.fs.readFile('package.json', 'utf-8')
@@ -61,7 +102,7 @@ watch(textareaValue, (value) => {
       <textarea v-model="textareaValue" flex-1 resize-none border-1 p-2 />
       <iframe :src="iframeSrc" flex-1 border-1 />
     </div>
-    <div ref="terminalEl" mt-3 h-428px border-1 />
+    <div ref="terminalEl" mt-3 max-h-428px overflow-hidden border-1 />
   </div>
 </template>
 
